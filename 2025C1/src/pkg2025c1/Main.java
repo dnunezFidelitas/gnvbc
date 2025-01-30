df60d6e1df90e9a560e35e64baa639f25ab9bde8
dnunezFidelitas <133621200+dnunezFidelitas@users.noreply.github.com>
2025-01-29 18:31:48 -0600

    Add files via upload

---
+++ 2025C1/src/pkg2025c1/Main.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2025c1;
+
+import javax.swing.JOptionPane;
+
+
+
+/**
+ *
+ * @author viti
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int miPrimeraVariable;
+        String miSegundaVariable;
+        
+        miPrimeraVariable=99;
+        miSegundaVariable="CLASE 1 GRUPO";
+        
+        String variable="Hahaha";    
+//        String Variable="Hahaha"; 
+//        String VARIABLE="Hahaha"; 
+
+        String nombre;
+        int edad;
+        float salario;
+        double temperatura;
+        char inicial;
+        
+        nombre=JOptionPane.showInputDialog("Por favor dame tu nombre");
+
+        //Esto es un comentario
+        
+        /**
+         * Esto tambien es un comentario
+         * 
+         */
+    }
+    
+}
